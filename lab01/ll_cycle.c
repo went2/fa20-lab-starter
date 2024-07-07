9874b8093b24ca64c240f25b29048b5403664120
WenTommy.Yu <wentao.yu5@gientech.com>
2024-07-07 23:07:32 +0800

    complete lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,13 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if(head == NULL) return 0;
+    struct node *tortoise = head;
+    struct node *hare = head;
+    while(hare->next != NULL && hare->next->next != NULL) {
+	hare = hare->next->next;
+	tortoise = tortoise->next;
+	if(hare == tortoise) return 1;
+    }
     return 0;
 }
--- lab01/no_segfault_ex.c
+++ lab01/no_segfault_ex.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
+#define ARR_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
     unsigned total = 0;
-    for (int j = 0; j < sizeof(a); j++) {
+    for (int j = 0; j < ARR_SIZE(a); j++) {
         total += a[j];
     }
     printf("sum of array is %d\n", total);
